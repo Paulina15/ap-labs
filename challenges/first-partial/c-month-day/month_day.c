aa4251d7f6bbdd2dd8fe6835d580fc6ea0785a9a
Paulina Palomera <A01635283@itesm.mx>
2021-09-09 02:44:59 +0000

    solve first-partial

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/* month_name:  return name of n-th month */
+char *month_name(int n) {
+
+   static char *name[] = {
+       "Illegal month",
+       "January", "February", "March",
+       "April", "May", "June",
+       "July", "August", "September",
+       "October", "November", "December"
+   };
+
+   return (n < 1 || n > 12) ? name[0] : name[n];
+}
+
+char *month_name(int n);
+int month, day;
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
 
 /* month_day function's prototype*/
 void month_day(int year, int yearday, int *pmonth, int *pday);
 
-int main() {
-    return 0;
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    int i, leap;
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; i < 13; i++){
+        if (yearday <= daytab[leap][i]){
+            *pmonth = i;
+            *pday = yearday;
+            return;
+        }
+        else {
+            yearday -= daytab[leap][i];
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 3){
+        printf("Faltan elementos");
+        return 0;
+    }
+    int year = atoi(argv[1]);
+    int yearday = atoi(argv[2]);
+    if (yearday > 366){
+        printf("A year has max 366 days, no more\n");
+        return 0;
+    }
+    int *pmonth, *pday;
+    pmonth = &month;
+    pday = &day;
+    month_day(year, yearday, pmonth, pday);
+    char *m = month_name(month);
+    printf("%s %d, %d\n", m, day, year); 
 }
--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -1,24 +1,32 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 156.
-
-// Package geometry defines simple types for plane geometry.
-//!+point
 package main
 
-import "math"
+import (
+	"math"
+	"math/rand"
+	"fmt"
+	"os"
+	"strconv"
+	"sort"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct{ x, y float64 }
+
+func (p Point) X() float64 {
+	return p.x;
+}
+
+func (p Point) Y() float64 {
+	return p.y;
+}
 
 // traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
 }
 
 //!-point
@@ -39,7 +47,64 @@ func (path Path) Distance() float64 {
 	return sum
 }
 
+//!-path
+
 func main() {
+	if len(os.Args) == 2{
+		if sides, err := strconv.Atoi(os.Args[1]); err == nil {
+			if (sides > 2) {
+				path := generateForm(sides) // Generate shape
+				print_Form(path) 
+			} else {
+				fmt.Printf("ERROR Cant create a shape from %v side(s)\n", sides)	
+			}
+		} else {
+			fmt.Println("ERROR Usage go run geometry.go <sides>")	
+		}
+	} else {
+		fmt.Println("ERROR Usage go run geometry.go <sides>")	
+	}
 }
 
-//!-path
+type orientationP Path
+
+func (a orientationP) Len() int {
+	 return len(a)
+}
+func (a orientationP) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a orientationP) Less(i, j int) bool { 
+	return math.Atan2(a[i].Y(), a[i].X()) < math.Atan2(a[j].Y(), a[j].X()) 
+}
+
+func generateForm(sides int) Path{
+	min := -100.00
+	max := 100.00
+	points := make([]Point, sides)
+	closedForm := make([]Point, sides+1)
+
+	for i := 0; i < sides; i++ {
+		x := min + rand.Float64() * float64(max - min)
+		y := min + rand.Float64() * float64(max - min)
+		points[i] = Point{x, y}
+	}
+	sort.Sort(orientationP(points))
+	copy(closedForm, points)
+	closedForm[sides] = points[0];
+
+	return closedForm
+}
+
+func print_Form(path Path) {
+	fmt.Printf("- Generating a [%v] side figure\n", len(path)-1)
+	fmt.Printf("- Vertices of the figure\n")
+	for _, el := range path[:len(path)-1] {
+		fmt.Printf("  - (%.2f,  %.2f)\n", el.X(), el.Y())
+	}
+	fmt.Printf("- Figure's Perimeter\n  - ")
+	for i := range path {
+		if i > 0 && i < len(path)-1 {
+			fmt.Printf("%.2f + ", path[i-1].Distance(path[i]))
+		}
+	}
+	fmt.Printf("%.2f = %.2f\n", path[len(path)-2].Distance(path[len(path)-1]), path.Distance())
+}
