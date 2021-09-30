bf6874fbc4f634c3fb1d1b823f14e114388fe624
Paulina Palomera <A01635283@itesm.mx>
2021-09-30 18:09:14 +0000

    solve str-lib

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+    if (argc==4 && strcmp(argv[1],"-find")==0){
+         int buscar = mystrfind(argv[2], argv[3]);
+        if (buscar > -1) {
+            printf("['%s'] el string fue econtrado en la [%d] posicion\n", argv[3], buscar);
+        } else {
+            printf("El input no fue encontrado \n");
+        }
+        return 0;  
+    }
+    else if (argc==4 && strcmp(argv[1],"-add")==0){
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *added_word = mystradd(argv[2],argv[3]);
+        printf("New String          : %s\n", added_word);
+        printf("New length          : %d\n", mystrlen(added_word));
+        return 0;
+    }
+    else if (argc==3 && strcmp(argv[1],"-add")==0){
+        printf("Error. No tenemos ningun string para agregar. \n");
+    }
+    else if (argc==3 && strcmp(argv[1],"-find")==0){
+        printf("Error. No tenemos un string que encontrar. \n");
+    }
+    else {
+        printf("Error. Lo que introduciste como parametro es incorrecto \n");
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,53 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i = 0;
+    char c;
+    while(c=str[i]!='\0'){
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    
+    char *final = origin + mystrlen(origin);
+
+    while(*addition!='\0') {
+        *final++ = *addition++;
+    }
+    *final = '\0';
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+
+    int len = mystrlen(origin);
+    int len_substr = mystrlen(substr);
+
+    int  i = 0,
+         j = 0;
+
+    int  cont = 0;
+
+    while(i<len){
+        if (origin[i]==substr[0]){
+            j = 0;
+            cont = i;
+            while(j<len_substr){
+                if (origin[cont]==substr[j]){
+                    cont++;
+                }
+                else{
+                    break;
+                }
+                j++;
+            }
+            if (j==len_substr){
+                return i+1;
+            }
+        }
+        i++;
+    }
+    return -1;
 }
